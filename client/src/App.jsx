6bdab0bd56447281aedff68c2f5c4f86491173bc
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-07-07 12:15:39 +0530

    feat: Add Login component and styles
    This commit adds a new `Login` component to the client application. The `Login` component allows users to enter their email and password and click a login button. It also includes CSS styles for the component and its related elements.

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+
+function App() {
+
+  return (
+   <div>
+    <SignUp />
+    <Login />
+   </div>
+  )
+}
+
+export default App
