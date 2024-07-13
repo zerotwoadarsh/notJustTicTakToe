374414c27e5100f8ea44da2cd892729224c54b75
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-07-13 20:41:06 +0530

    ok

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   return (
     <div>
       <label>Login</label>
-      <input placeholder='Email' onChange={(event)=>{setUserName(event.target.value)}}/>
+      <input placeholder='Username' onChange={(event)=>{setUserName(event.target.value)}}/>
       <input placeholder='Password' onChange={(event)=>{setPassword(event.target.value)}}/>
       <button onClick={login}>Login</button>
     </div>
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { useState } from 'react';
+import axios from 'axios'
+import cookie from 'Universal-cookie'
 
 
 const SignUp = () => {
+  const cookies = new cookie();
   const [user, setUser] = useState(null);
   firstName :{}
 
-  const signUp = () => {};
+  const signUp = () => {
+    axios.post('http://localhost:3001/signup', user).then((res) => {
+      const { userId, token, fullName, username, hashPassword } = res.data;
+      cookies.set("token", token);
+    });
+  };
   return (
     <div>
       <label>Sign Up</label>
       <input placeholder='First Name' onChange={(event)=>{setUser({...user, firstName: event.target.value})}} />
       <input placeholder='Last Name' onChange={(event)=>{setUser({...user, lastName: event.target.value})}}/>
-      <input placeholder='Email' onChange={(event)=>{setUser({...user, email: event.target.value})}}/>
+      <input placeholder='Username' onChange={(event)=>{setUser({...user, username: event.target.value})}}/>
       <input placeholder='Password' onChange={(event)=>{setUser({...user, password: event.target.value})}}/>
       <button onClick={signUp}>Sign Up</button>
     </div>
